2b7dbf4a0e0e7a3877122b6ef03eb8d07bd29f7a
Bibubobee <72390136+Bibubobee@users.noreply.github.com>
2021-10-17 16:40:18 -0300

    Entrega 1.0

---
+++ Entrega/Animal.h
@@ -0,0 +1,32 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+typedef struct Animal{
+    void* fuerza; //El NULL check de todo este codigo es ver si fuerza == NULL.
+    char tipo_fuerza;
+    void* velocidad;
+    char tipo_velocidad;
+    void* resistencia;
+    char tipo_resistencia;
+    void (*reproduccion)(struct Animal*, struct Animal*, struct Animal*);
+    void (*comerHuir)(struct Animal*, struct Animal*);
+    int coordenadas[2]; //Agregué coordenadas internas para facilitar algunas funciones.
+                        //Al colocarse en el Mundo se ponen como (y, x)
+} Animal;
+
+int TraductorValores (void* val, char tipo) ; //Funcion auxiliar, queria dejar mas ordenado eso.
+void CrearTransimperativer (Animal* a ) ;
+void Borrar (Animal* a ) ;
+void MostrarAnimal (Animal* a ) ;
+void Reproducir (Animal* a1 , Animal* a2 , Animal* hijo ) ;
+void Combatir (Animal* a1 , Animal * a2 ) ;
+int Comparar (Animal* a1 , Animal * a2 ) ;
+void BorrarMundo (Animal** Mundo ) ;
+void MostrarMundo (Animal** Mundo ) ;
+void ReproduccionSimple (Animal* a1 , Animal* a2 , Animal* hijo ) ;
+void ReproduccionCruzada (Animal* a1 , Animal* a2 , Animal* hijo ) ;
+void ComerSiempre (Animal* a1 , Animal * a2 ) ;
+void HuirSiempre (Animal* a1 , Animal * a2) ;
+void ComerAleatorio (Animal* a1 , Animal * a2 ) ;
+
+#endif
---
+++ Entrega/README.txt
@@ -0,0 +1,14 @@
+NOMBRE :: GABRIEL SEBASTIÁN ORTIZ ÁNGEL
+ROL    :: 202073535-2
+
+Detalles de Compilacion:
+Para compilar el programa utilice el comando 'make simulacion'
+en la consola, y para correrlo use './simulacion.o'. Además para
+testeos con Valgrind se usó 'valgrind --leak-check=yes ./simulacion.o'
+
+Comentarios sobre el código:
+Se agregó el parametro "coordenadas" al struct para obtener las coordenadas
+comodamente, al colocarse en las matrices se colocan del modo (y,x).
+Ademas se agregó a Animal.h la función TraductorValores para facilitar la conversion
+a int. Por ultimo se agregaron los archivos Mundo.h y Mundo.c, donde se controla
+toda la simulación para las matrices, tanto la del Mundo como la auxiliar.
